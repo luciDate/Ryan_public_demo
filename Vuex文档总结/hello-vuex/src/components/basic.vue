cbe7e665e81881c148846796aedd9fd573e9e178
Ryan <lucidatewithcode@gmail.com>
2019-08-13 01:08:15 +0800

    fix

--- Vuex文档总结/hello-vuex/src/components/basic.vue
+++ Vuex文档总结/hello-vuex/src/components/basic.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="basic">
     <h1>Hello Vue</h1>
-    <h2>{{count}}</h2>
+    <h2>{{doneTodos}}</h2>
+    <h3>{{doneTodosCount}}</h3>
+    <h4>{{getTodoById(2)}}</h4>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
       message: "小明"
     };
   },
-  computed: mapState([
+  computed: mapGetters([
     // 映射 this.count 为 store.state.count
-    "count"
+    "doneTodos",
+    "doneTodosCount",
+    "getTodoById"
   ])
 };
 </script>
--- Vuex文档总结/hello-vuex/src/main.js
+++ Vuex文档总结/hello-vuex/src/main.js
@@ -5,7 +5,23 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    count: 3
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    //getters调用自身函数
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length;
+    },
+    //getters传参
+    getTodoById: state => id => {
+      return state.todos.find(todo => todo.id === id);
+    }
   }
 });
 
@@ -13,5 +29,10 @@ Vue.config.productionTip = false;
 
 new Vue({
   store,
+  data() {
+    return {};
+  },
   render: h => h(App)
 }).$mount("#app");
+
+//console.log(store.getters.getTodoById(2));
